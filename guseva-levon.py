88c7c2f55aec4b8f263d5d702337197ab40ce253
Vasiliy Olekhov <olekhov@gmail.com>
2019-09-11 21:11:10 +0300

    elec.db

--- guseva-levon.py
+++ guseva-levon.py
@@ -1,48 +1,63 @@
 #!/usr/bin/env python3.7
 
-import numpy as np
 import matplotlib.pyplot as plt
-import csv
 import pdb
 import sqlite3
 
 db=sqlite3.connect("elec.db")
-q=f"""SELECT u.uik_name, p_guseva.row_value,p_smirnov.row_value, p_smirnov.row_value>p_guseva.row_value
-FROM UIKS u, UIK_Protocol p_guseva, UIK_Protocol p_smirnov, Candidates guseva, Candidates smirnov
+q=f"""SELECT 
+u.uik_name, 
+p_guseva.row_value*100.0/(p3.row_value+p3a.row_value+p4.row_value),
+p_smirnov.row_value*100.0/(p3.row_value+p3a.row_value+p4.row_value), 
+p_smirnov.row_value>p_guseva.row_value,
+(p3.row_value+p3a.row_value+p4.row_value)
+FROM UIKS u, UIK_Protocol p_guseva, UIK_Protocol p_smirnov, 
+UIK_Protocol p3,
+UIK_Protocol p3a,
+UIK_Protocol p4,
+Candidates guseva, Candidates smirnov
 WHERE
 p_guseva.uik_id = u.id AND
 p_smirnov.uik_id = u.id AND
+p3.uik_id = u.id AND
+p3a.uik_id = u.id AND
+p4.uik_id = u.id AND
+p3.row_number='3' AND
+p3a.row_number='3а' AND
+p4.row_number='4' AND
 guseva.candidate_name like 'Гусева %' AND
 smirnov.candidate_name like 'Смирнов %' AND
 p_guseva.candidate_id = guseva.id AND
 p_smirnov.candidate_id = smirnov.id
-ORDER BY 4,3"""
+ORDER BY 3"""
 c=db.cursor()
 c.execute(q)
 data = c.fetchall()
 #pdb.set_trace()
 
-group_names  =  list([ ' '.join(x[0].split(' ')[1:3]) for x in data])
+group_names  =  list([ ' '.join(x[0].split(' ')[1:3])+f"({x[4]})" for x in data])
 group_guseva =  list([int(x[1]) for x in data])
 group_smirnov = list([int(x[2]) for x in data])
-group_win = list([(600 if x[3] else 0) for x in data])
+group_win = list([(100 if x[3] else 0) for x in data])
 #pdb.set_trace()
 
 #plt.rcdefaults()
-fig,ax_w=plt.subplots(figsize=(6,20),dpi=150)
+fig,ax_w=plt.subplots(figsize=(3,4),dpi=150)
 ax_g = ax_w.twiny()
 ax_l = ax_w.twiny()
-ax_w.set_xlim(xmin=0,xmax=600)
+ax_w.set_xlim(xmin=0,xmax=100)
 #ax_w.set_visible(False)
-ax_w.barh(group_names, group_win, color=(0.8,1.0,0.8))
+#ax_w.barh(group_names, group_win, color=(0.8,1.0,0.8))
 
-ax_g.set_xlim(xmin=0,xmax=600)
-ax_g.barh(group_names, group_guseva, height=0.7, label = "Гусева", color=(0.8,0.8,0.8))
+ax_g.set_xlim(xmin=0,xmax=100)
+ax_g.barh(group_names, group_guseva, height=1.0, label = "Гусева", color=(0.8,0.8,0.8))
 ax_g.xaxis.tick_bottom()
+ax_g.yaxis.set_ticklabels([])
 
-ax_l.set_xlim(xmin=600,xmax=0)
-ax_l.barh(group_names, group_smirnov, height=0.7, label = "Левон", color=(1,0,0))
+ax_l.set_xlim(xmin=100,xmax=0)
+ax_l.barh(group_names, group_smirnov, height=1.0, label = "Левон", color=(1,0,0))
 ax_l.xaxis.tick_top()
+ax_l.yaxis.set_ticklabels([])
 
 
 #ax.set_xlim(xmin=0,xmax=6000)
